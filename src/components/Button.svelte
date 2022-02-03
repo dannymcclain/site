25d56e9a3849ca79e32e895e380b236283bec8db
Danny McClain <dannyxmcclain@gmail.com>
2022-02-02 21:31:19 -0600

    adding icons and button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    export let name:String = '';
+    export let label:String = '';
+    export let url:string = '';
+
+    let hovering;
+
+	function enter() {
+		hovering = true;
+	}
+
+	function leave() {
+		hovering = false;
+	}
+</script>
+
+<a on:mouseenter={enter} on:mouseleave={leave}
+href={url} 
+class="button">
+    <svg class="icon">
+        {#if hovering}
+            <use href="icons.svg#{name}-filled"></use>
+        {:else}
+            <use href="icons.svg#{name}-line"></use>
+            {/if}
+    </svg>
+    {label}
+</a>
+
+<style>
+    .button {
+        display: inline-flex;
+        flex-direction: row;
+        align-items: center;
+        flex-basis: auto;
+        padding: 12px 16px;
+        background: transparent;
+        border: 2px solid rgba(0, 68, 255, 0.12);
+        border-radius: 200px;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 1rem;
+        line-height: 1rem;
+        letter-spacing: -0.02em;
+        text-decoration: none;
+        color: var(--accent);
+        /* transition: background-color 150ms ease-in-out, border-color 150ms ease-in-out, color 150ms ease-in-out; */
+    }
+    .button:hover {
+        background: var(--accent);
+        border-color: var(--accent);
+        color: #fff;
+    }
+    .icon {
+        width: 1.5rem;
+        height: 1.5rem;
+        margin-right: 0.5rem; 
+    }
+    </style>
+    
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,61 @@
+<script>
+
+    import Button from '../components/Button.svelte'
+
+    let style= 'line'
+    function handleMouseOver(e) {
+        style= 'filled'
+    }
+    function handleMouseOut(e) {
+        style= 'line'
+    }
+</script>
+
+<style>
+    @import url('https://rsms.me/inter/inter.css');
+
+    :global(:root){
+        --accent: #0044ff;
+    }
+    :global(*),
+    :global(*::after),
+    :global(*::before) {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    :global(body){
+        font-family: 'Inter', system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+        font-size: 16px;
+        color: var(--accent);
+    }
+
+    main {
+        padding: 40px 20px;
+    }
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    @media screen and (min-width: 768px){
+        main {
+            padding: 120px;
+        }
+    }
+</style>
+
+<main>
+    
+    <nav>
+        <Button name='smile' label='Danny McClain' url='/'></Button>
+        <Button name='info' label='Info' url='/info'></Button>
+    </nav>
+
+    <slot></slot>
+
+</main>
