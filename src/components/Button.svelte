acbe8112e2474cb6a61d416007e485eab09abc99
Danny McClain <dannyxmcclain@gmail.com>
2022-02-03 10:08:49 -0600

    editing button nav behavior

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
     export let name:String = '';
     export let label:String = '';
     export let url:string = '';
+
+    function handleClick() {
+        goto(url)
+    }
 </script>
 
-<a
+<a on:click={handleClick}
     href={url} 
     class="button">
         <div class="icon-image">
@@ -18,29 +23,7 @@
     {label}
 </a>
 
-<style>
-    /* .icon-image {
-        height:1.5rem;
-        width:1.5rem;
-        position: relative;
-        display:inline-block;
-        margin-right: .5em
-    }
-    .icon-image svg {
-        top:0;
-        left:0;
-    } */
-    .icon-image {
-        position: relative;
-        height: 1.5rem;
-        width: 1.5rem;
-        margin-right: .5rem;
-    }
-    .icon {
-        position: absolute;
-        top:0;
-        left:0;
-    }
+<style>   
     .button {
         display: inline-flex;
         flex-direction: row;
@@ -59,28 +42,40 @@
         color: var(--accent);
         transition: background-color 100ms linear, border-color 100ms linear, color 100ms linear;
     }
+    
     .button:hover {
         background: var(--accent);
         border-color: var(--accent);
         color: #fff;
     }
+    
+    .icon-image {
+        position: relative;
+        height: 1.5rem;
+        width: 1.5rem;
+        margin-right: .5rem;
+    }
+
     .icon {
+        position: absolute;
+        top:0;
+        left:0;
         width: 1.5rem;
         height: 1.5rem;
         margin-right: 0.5rem; 
     }
+
     .filled {
         opacity:0;
         transform: scale(.2);
         transition: transform 400ms cubic-bezier(0.175, 0.885, 0.32, 1.275), opacity 200ms linear;
-        /* transition-delay: 200ms; */
     }
-    
     .line {
         opacity: 1;
         transform: scale(1);
         transition: transform 400ms cubic-bezier(0.175, 0.885, 0.32, 1.275), opacity 200ms linear;
     }
+    
     .button:hover .filled {
         opacity: 1;
         transform: scale(1);
